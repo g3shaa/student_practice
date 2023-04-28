4e50e0648b20cb909dc72d27c835547552e76e90
Geshata <64840882+g3shaa@users.noreply.github.com>
2023-04-28 09:52:13 +0300

    Added tasks

---
+++ C#_Tasks/Factorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int number = 8;
+        int result = Factorial(number);
+        Console.WriteLine("The factorial of {0} is {1}.", number, result);
+    }
+
+    static int Factorial(int n)
+    {
+        if (n == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return n * Factorial(n - 1);
+        }
+    }
+}
---
+++ C#_Tasks/task1_tema5/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] array = { 1, 3, 4, 17, 19, 17, 83, 123, 133, 212 };
+int ind = -1; // променено от -2 на -1
+Console.WriteLine("Enter number for val=");
+int val = int.Parse(Console.ReadLine()); // променено от Console.ReadLn() на Console.ReadLine() и добавена ключовата дума "int"
+int left = 0, right = array.Length - 1; // променено от -2 на -1 и променено от -2 на -1
+while (left <= right)
+{
+    int mid = (left + right) / 2;
+    if (array[mid] == val)
+    {
+        ind = mid;
+        break; // добавен break за да не продължава цикъла след като е намерен индексът
+    }
+    else if (array[mid] < val)
+    {
+        left = mid + 1;
+    }
+    else
+    {
+        right = mid - 1;
+    }
+}
+Console.WriteLine(ind); // променено от Console.WriteLn() на Console.WriteLine()
