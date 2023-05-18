b4d3997bbd85d5f9f3e09fd5b8605d81c4342ce9
Geshata <64840882+g3shaa@users.noreply.github.com>
2023-05-18 18:57:03 +0300

    update

---
+++ C#_Tasks/MatrixDeterminant/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace MatrixDeterminant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[,]
+            {
+                {1, 2, 3},
+                {4, 3, 6},
+                {7, 8, 9}
+            };
+
+            int determinant = CalculateDeterminant(matrix);
+            Console.WriteLine("Determinant: " + determinant);
+        }
+
+        static int CalculateDeterminant(int[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+
+            if (rows != cols)
+            {
+                throw new ArgumentException("Матрицата не е квадратна");
+            }
+
+            if (rows == 1 && cols == 1)
+            {
+                return matrix[0, 0];
+            }
+
+            int determinant = 0;
+
+            for (int i = 0; i < rows; i++)
+            {
+                int sign = (i % 2 == 0) ? 1 : -1;
+
+                int[,] subMatrix = GetSubMatrix(matrix, i);
+
+                determinant += sign * matrix[0, i] * CalculateDeterminant(subMatrix);
+            }
+
+            return determinant;
+        }
+
+        static int[,] GetSubMatrix(int[,] matrix, int colToRemove)
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+
+            int[,] subMatrix = new int[rows - 1, cols - 1];
+
+            int rowIndex = 0;
+
+            for (int i = 0; i < cols; i++)
+            {
+                if (i == colToRemove)
+                {
+                    continue;
+                }
+
+                for (int j = 1; j < rows; j++)
+                {
+                    subMatrix[j - 1, rowIndex] = matrix[j, i];
+                }
+
+                rowIndex++;
+            }
+
+            return subMatrix;
+        }
+    }
+}
---
+++ C#_Tasks/binaryToDecimal/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace BinaryToDecimal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Въведи число в двоична бройна система: ");
+            string binaryNumber = Console.ReadLine();
+
+            int decimalNumber = ConvertBinaryToDecimal(binaryNumber);
+
+            Console.WriteLine("Резултат: " + decimalNumber);
+        }
+
+        static int ConvertBinaryToDecimal(string binaryNumber)
+        {
+            int decimalNumber = 0;
+            int power = 0;
+
+            for (int i = binaryNumber.Length - 1; i >= 0; i--)
+            {
+                if (binaryNumber[i] == '1')
+                {
+                    decimalNumber += (int)Math.Pow(2, power);
+                }
+
+                power++;
+            }
+
+            return decimalNumber;
+        }
+    }
+}
